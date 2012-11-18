70eac8f8aa9e8b2748df577eb2f2a7af65aa210d
Aleksandra Piasecka <alexa1401@gmail.com>
2012-11-18 15:28:43 +0100

    upgrade apps

--- Library/config/routes.rb
+++ Library/config/routes.rb
@@ -4,9 +4,16 @@ Library::Application.routes.draw do
       get 'crop'
     end
   end
+  
   # get 'books/:id/crop', to: 'books#crop', as: :crop_book
 
   get 'tags/:tag', to: 'books#index', as: :tag
+  
+  get   '/login', :to => 'sessions#new', :as => :login
+  match '/auth/:provider/callback', :to => 'sessions#create'
+  match '/auth/failure', :to => 'sessions#failure'
+  
+  get '/logout', :to => 'sessions#destroy'
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
