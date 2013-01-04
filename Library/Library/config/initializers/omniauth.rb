bf7d89fa93be58d21f77c5e74e869be01c28dc37
Aleksandra Piasecka <alexa1401@gmail.com>
2013-01-04 18:05:41 +0100

    xyz

--- Library/Library/config/initializers/omniauth.rb
+++
@@ -1,5 +0,0 @@
-Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :twitter, "NQqHRwLsdZSyva7Vf5Lhg", "EtgfVcMhLXS6OqHGvpTZy6Ts3009EmRfdL36AUmo0"
-  provider :github, "00f70fb9faa84372ddfd", "9223623e665163370c6fa734b4447b528d5b5e1f"
-  provider :facebook, "375843589176476", "275f4035665271aaf61cd8f057021e03"
-end
